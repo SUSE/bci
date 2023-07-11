077d674a3398fec712cd514ed7da5a2749befc97
Dirk Müller <dirk@dmllr.de>
2023-07-11 18:02:48 +0200

    Set basepath properly

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ module.exports = {
   swcMinify: true,
   images: {
     unoptimized: true
-  }
-  // if hosted in Github pages, since the format is user.github.io/REPO_NAME and Next.js expects the assets to live under the main path /, we need to rewrite it with the repository name.
-  // assetPrefix: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/project-template/',
-  // basePath: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/project-template',
+  },
+  //assetPrefix: 'https://opensource.suse.com/BCI',
+  basePath: '/BCI',
 }
